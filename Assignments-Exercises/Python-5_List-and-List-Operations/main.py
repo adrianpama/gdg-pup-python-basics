3a6d66d2291755eaacf880fdf61ca39512d68182
adrianpama <adrianppama@iskolarngbayan.pup.edu.ph>
2025-03-15 19:21:33 +0800

    Update main.py

--- Assignments-Exercises/Python-5_List-and-List-Operations/main.py
+++ Assignments-Exercises/Python-5_List-and-List-Operations/main.py
@@ -1,17 +1,15 @@
 # Step 1: Create a list of integers
-my_list = [5, 3, 8, 1]
+my_list = [1, 2, 3, 4, 5, 6, 7, 9, 8]
 print("Original list:", my_list)
 
 # Step 2: Add an element to the list
-my_list.append(6)
+my_list.append(10)
 print("List after adding an element:", my_list)
 
 # Step 3: Remove an element from the list
-my_list.remove(8)
+my_list.remove(5)
 print("List after removing an element:", my_list)
 
 # Step 4: Sort the list in ascending order
 my_list.sort()
 print("List after sorting:", my_list)
-
-
